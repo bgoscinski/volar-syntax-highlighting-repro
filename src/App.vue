db5de1af4100287fbecda0495c10d27078873753
Bartosz Gościński <bartosz.goscinski@itcraft.pl>
2022-05-09 20:44:21 +0200

    repro

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- try uncommenting the line below -->
+  <!-- <Svg /> -->
+  <Svg></Svg>
+</template>
+
+<script setup>
+import Svg from './Svg.vue';
+
+3 + 2;
+someFn();
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
